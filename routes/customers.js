e41c3c6ae5014f396c46307f9f2bad1334ff85d5
prateek951 <prince.cenation@gmail.com>
2018-10-31 20:17:02 +0530

    get a single customer

--- routes/customers.js
+++ routes/customers.js
@@ -1,6 +1,7 @@
 const errors = require('restify-errors');
 const Customer = require('../models/Customer');
 const HTTP_STATUS_CODES = require('http-status-codes');
+
 module.exports = server => {
     //Get Customers
     server.get('/customers', async (req, res, next) => {
@@ -12,7 +13,19 @@ module.exports = server => {
             return next(new errors.InvalidContentError(error));
         }
     });
+    
 
+    //Get a single customer
+    server.get('/customers/:id', async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const customer = await Customer.findById({_id : id});
+            res.status(HTTP_STATUS_CODES.OK).send(customer);
+        } catch (error) {
+            return next(new errors.IResourceNotFoundError(`There is no customer with the id of ${id}`));
+        }
+
+    });
 
     //Add a new customer 
     server.post('/customers',async (req, res, next) => {
